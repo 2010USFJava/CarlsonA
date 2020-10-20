6a58abfd1badf384e40dd904b9af778e7c01f47b
Anna <mounkeygirl@yahoo.com>
2020-10-20 15:59:33 -0500

    Updated Id num for demon

--- BeanFunWithCarlsonA/src/com/Carlson/Demon.java
+++ BeanFunWithCarlsonA/src/com/Carlson/Demon.java
@@ -6,15 +6,27 @@ public class Demon {
 	private String name;
 	private int age;
 	private Toy hauntedToy=new Toy();
+	private int id;
+	
+	//global
+	private static int nextId=1;
 	
 	
 	//constructors
-	public Demon() {}
+	public Demon() {
+		updateId();
+	}
 	public Demon(String name, int age, Toy hauntedToy) {
+		updateId();
 		this.name=name;
 		this.age=age;
 		this.hauntedToy=hauntedToy;
 		
+	}
+	
+	private void updateId() {
+		id=nextId;
+		nextId++;
 	}
 	
 	//getters and setters
@@ -33,7 +45,7 @@ public class Demon {
 	//to string
 	@Override
 	public String toString() {
-		return "Demon\nName: "+name+"\nAge: "+age+"\nHauntedToy: "+hauntedToy.getName()+"\n";
+		return "Demon #"+id+"\nName: "+name+"\nAge: "+age+"\nHauntedToy: "+hauntedToy.getName()+"\n";
 		
 	}
 
