c10fd31f16323c28953ddead07bf8278d9fb783d
mounkeygirl <mounkeygirl@yahoo.com>
2020-10-21 16:55:01 -0500

    Pro 0 worked on name with spaces resonses

--- Training Project 0/Project 0 Java/src/com/Revature/Main.java
+++ Training Project 0/Project 0 Java/src/com/Revature/Main.java
@@ -1,22 +1,19 @@
 package com.Revature;
 
-import java.util.Scanner;
 
 public class Main {
 	
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
+		User user = new User();
 		
-		String name;
+		System.out.println("Hello world");
 		
+		user.setFirstName("Carl");
+		user.setFirstName("ASda asda");
+		System.out.println(user.getFirstName());
 		
-		
-		System.out.println("Please enter your name:");
-		name=scan.nextLine();
-		
-		System.out.println("Hello "+name);
 
 	}
 
--- Training Project 0/Project 0 Java/src/com/Revature/User.java
+++ Training Project 0/Project 0 Java/src/com/Revature/User.java
@@ -33,7 +33,7 @@ public class User {
 		//if
 		if (hasAdditionalMiddleNames==true) {
 //			Run through each item and add it with a space to the new output
-			
+			 
 			
 		}
 		return output;
@@ -42,51 +42,111 @@ public class User {
 	}
 	
 	public void setFirstName(String firstName) {
-		this.firstName=checkName(firstName);
+		this.firstName=checkName(firstName, "First Name");
 	}
 	
 	//checks if the name has spaces between it and breaks it up
-	private String checkName(String name) {
-		name= name.trim();
-		String[]nameParts=name.split(" ");
-		int arraySize=nameParts.length;
-		if (arraySize==1) {
-			return name;
-		} else {
-			String dashOption="";
-			String removeSpace="";
-			for(int i=0;i<arraySize;i++) {
-				dashOption+="-"+nameParts[i].trim();
-				removeSpace+=nameParts[i].trim();
-			}
-			
-			System.out.println("There are extra spaces in this part of the name. \nWhat would you like to do?\n"
-					+ "0 : Re-enter Name\n"
-					+ "1 : Use "+dashOption+"\n"
-					+ "2 : Use "+removeSpace+"\n");
-			
-			String responseString = scan.nextLine();
-			int answerNumber;
+	private String checkName(String name,String partOfName) {
+
 			boolean usableAnswer=false;
 			
-			try{
-				answerNumber=Integer.parseInt(responseString);
-				usableAnswe
-			}catch(NumberFormatException e){
-				do {
-					System.out.println("That was not a number, please try again.");
+			do {
+				//remove white space on ends
+				name= name.trim();
+				//Make sure first letter is upper case while rest is lower case
+				name=name.substring(0,1).toUpperCase()+name.substring(1).toLowerCase();
+				String[]nameParts=name.split(" ");
+				int arraySize=nameParts.length;
+				if (arraySize==1) {
+					return name;
+				} else {
+				String dashOption=dashifyName(nameParts);
+				String removeSpace=removeSpacesFromName(nameParts);
 					
+					System.out.println("There are extra spaces in the "+partOfName+" \""+name+"\". \nWhat would you like to do?\n"
+							+ "0 : Re-enter Name\n"
+							+ "1 : Use "+dashOption+"\n"
+							+ "2 : Use "+removeSpace+"\n");
 					
-				} while(usableAnswer==false);
+				String responseString = scan.nextLine();
 				
+				int answerNumber=0;
+//				If the catch block does not initiate, this will be true and the loop will break
+				usableAnswer=true;
+				try{
+					answerNumber=Integer.parseInt(responseString);
+					
+//					Also check if number is in range
+					if(answerNumber==1) {
+						name=dashOption;
+					} else if(answerNumber==2) {
+						name=removeSpace;
+					} else if(answerNumber==0) {
+						usableAnswer=false;
+						System.out.println("Please reenter the "+partOfName+":\n");
+						name = scan.nextLine();
+						
+					} else {
+						System.out.println(responseString+" is not a vailid choice. Please try again.");
+						usableAnswer=false;
+					}
+					
+				}catch(NumberFormatException e){
+					System.out.println("That was not a number, please try again.");	
+					usableAnswer=false;
+				}
 				
-			}
+			} 
 			
-		}
-		
+		} while(usableAnswer==false);
 		return name;
+	}
+	
+
+	
+	//internal use String Modification methods
+	
+	private String[] splitName(String name) {
+		name= name.trim();
+		String[]nameParts=name.split(" ");
+		return nameParts;
 		
+	}
+	
+	private String dashifyName(String name) {
+		return(dashifyName(splitName(name)));
+	}
+	
+	private String dashifyName(String[]nameParts) {
+		int arraySize=nameParts.length;
+		String dashOption="";
 		
+		for(int i=0;i<arraySize;i++) {
+			if(i==0) {
+
+				
+				dashOption+=nameParts[i].substring(0,1).toUpperCase()+nameParts[i].substring(1).toLowerCase();
+				
+			}else {
+				dashOption+="-"+nameParts[i].substring(0,1).toUpperCase()+nameParts[i].substring(1).toLowerCase();				
+			}
+		}
+		return dashOption;
+		
+	}
+	
+	private String removeSpacesFromName(String name) {
+		return(removeSpacesFromName(splitName(name)));
+		
+	}
+	
+	private String removeSpacesFromName(String[]nameParts) {
+		int arraySize=nameParts.length;
+		String removeSpace="";
+		for(int i=0;i<arraySize;i++) {
+			removeSpace+=nameParts[i].trim();
+		}
+		return removeSpace;
 	}
 	
 	
