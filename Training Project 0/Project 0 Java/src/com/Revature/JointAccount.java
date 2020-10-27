7e6f761255948bf4e0d3d9b17b15bf5ef8a2c5bc
mounkeygirl <mounkeygirl@yahoo.com>
2020-10-27 11:11:04 -0500

    Added more Joint Account Constructors

--- Training Project 0/Project 0 Java/src/com/Revature/JointAccount.java
+++ Training Project 0/Project 0 Java/src/com/Revature/JointAccount.java
@@ -8,5 +8,21 @@ public class JointAccount extends Account {
 		super(accountHolder);
 		this.secondAccountHolder = secondAccountHolder;
 	}
+	
+	public JointAccount(String account1FirstName,String account1LastName,String account2FirstName,String account2LastName) {
+		super(account1FirstName,account1LastName);
+		secondAccountHolder=new Customer(account2FirstName,account2LastName);
+		
+		
+	}
+	
+	public JointAccount(String account1FirstName,String account1MiddleName,String account1LastName,String account2FirstName, String account2MiddleName,String account2LastName) {
+		super(account1FirstName,account1MiddleName,account1LastName);
+		secondAccountHolder=new Customer(account2FirstName,account1MiddleName,account2LastName);
+		
+		
+	}
+
+
 
 }
